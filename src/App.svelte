aee4953c77ccab8f0fa99e7bb9a52b910dda0e2b
GHOST <ghostdevbusiness@gmail.com>
2020-12-07 20:24:08 +0000

    feat: implimented scss

--- src/App.svelte
+++ src/App.svelte
@@ -6,7 +6,7 @@
 	<h1>Hello World!</h1>
 </main>
 
-<style>
+<style lang="scss">
 	main {
 		text-align: center;
 		display: flex;
@@ -14,14 +14,15 @@
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
+
+		h1 {
+			color: #ff3e00;
+			text-transform: uppercase;
+			font-size: 4em;
+			font-weight: 100;
+		}
 	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
 
 	@media (min-width: 640px) {
 		main {
