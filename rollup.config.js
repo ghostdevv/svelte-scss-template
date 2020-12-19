4e36b224f3387929e105135947104e666c54e5a4
GHOST <ghostdevbusiness@gmail.com>
2020-12-19 12:40:57 +0000

    feat: created a svelte.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,6 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
-import autoPreprocess from 'svelte-preprocess';
 import css from 'rollup-plugin-css-only';
 
 const production = !process.env.ROLLUP_WATCH;
@@ -42,18 +41,7 @@ export default {
         file: 'public/build/bundle.js',
     },
     plugins: [
-        svelte({
-            compilerOptions: {
-                // enable run-time checks when not in production
-                dev: !production,
-            },
-            sass: true,
-            // Setup the preproccess for svelte, and tell it to use postcss
-            preprocess: autoPreprocess({
-                postcss: require('./postcss.config.js'),
-                defaults: { style: 'postcss' },
-            }),
-        }),
+        svelte(require('./svelte.config.js')),
 
         // we'll extract any component CSS out into
         // a separate file - better for performance
